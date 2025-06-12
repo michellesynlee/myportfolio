ac422f77a208b5e1dc71519fbbc607cefaeeea0e
Michelle Lee <73571012+michellesynlee@users.noreply.github.com>
2025-06-12 17:54:53 -0400

    Trying to rework for github

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')  # Use '.' to point to the current folder
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/projects')
 def projects():
